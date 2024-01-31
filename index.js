c7ac5cce16f12571f6b3d02d044373e809925d21
merajnidhiimpex <157499444+merajnidhiimpex@users.noreply.github.com>
2024-01-31 15:39:09 +0530

    Update index.js
    updatess

--- index.js
+++ index.js
@@ -26,10 +26,4 @@ app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`)
 })
 
-// ssh-keygen -t ed25519 -C "merajnidhiimpex@gmail.com"
-
-// cat ~/.ssh/id_ed25519.pub | clip
-
-
-// ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEcFF1D+SVtLG/d77/eigDk16MsclQ51PvOLXniVFI9p merajnidhiimpex@gmail.com
 
